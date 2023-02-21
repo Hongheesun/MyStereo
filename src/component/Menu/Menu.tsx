38b5d458157b05e3f853a0a6c892dc2fe2677910
홍희선 <hia990618@gmail.com>
2023-02-21 19:39:50 +0900

    feat: user-sound로 버튼 효과음 추가

--- src/component/Menu/Menu.tsx
+++ src/component/Menu/Menu.tsx
@@ -1,13 +1,22 @@
 import * as Styled from "./Menu.style";
+import useSound from "use-sound";
 import { VideoTape } from "../../assets/images";
 import MenuData from "../../assets/json/menu.json";
+import { ButtonSound } from "../../assets/sounds";
 
 function Menu() {
+  const [play] = useSound(ButtonSound);
+
   return (
     <Styled.Container>
-      {MenuData.map((menu) => {
+      {MenuData.map((menu, id) => {
         return (
-          <Styled.VideoTapeWrapper>
+          <Styled.VideoTapeWrapper
+            key={id}
+            onClick={() => {
+              play();
+            }}
+          >
             <Styled.MenuText>{menu.menu}</Styled.MenuText>
             <Styled.VideoTape src={VideoTape} alt="vdeoTape" />
           </Styled.VideoTapeWrapper>
