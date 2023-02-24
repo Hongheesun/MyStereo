f56fc79ce2b7314d4ab7da3f0cb323c0ce0cb8cf
홍희선 <hia990618@gmail.com>
2023-02-24 12:57:28 +0900

    design: container markup

--- src/component/Category/Category.style.ts
+++ src/component/Category/Category.style.ts
@@ -1,10 +1,5 @@
 import styled from "styled-components";
 
-interface IAreaProps {
-  isDraggingFromThis: boolean;
-  isDraggingOver: boolean;
-}
-
 interface IContainerProps {
   boardId: string;
 }
@@ -12,13 +7,13 @@ interface IContainerProps {
 export const Container = styled.div<IContainerProps>`
   position: ${(props) =>
     props.boardId === "categories" ? "relative" : "absolute"};
-  top: ${(props) => (props.boardId === "categories" ? 0 : "130px")};
-  right: ${(props) => (props.boardId === "categories" ? 0 : "43.5%")};
+  top: ${(props) => props.boardId === "play" && "130px"};
+  right: ${(props) => props.boardId === "play" && "43.5%"};
   display: flex;
   flex-wrap: wrap;
   justify-content: center;
-  align-items: ${(props) => (props.boardId === "categories" ? 0 : "center")};
-  width: ${(props) => (props.boardId === "categories" ? "auto" : "138px")};
-  height: ${(props) => (props.boardId === "categories" ? "auto" : "71px")};
+  align-items: ${(props) => props.boardId === "play" && "center"};
+  width: ${(props) => props.boardId === "play" && "138px"};
+  height: ${(props) => props.boardId === "play" && "71px"};
   margin-top: 100px;
 `;
