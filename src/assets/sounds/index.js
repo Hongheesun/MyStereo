aa3c9789f2da06caab6c559d92c42aaedded079b
홍희선 <hia990618@gmail.com>
2023-02-27 02:02:47 +0900

    add: music 추가

--- src/assets/sounds/index.js
+++ src/assets/sounds/index.js
@@ -1,6 +1,9 @@
 import ButtonSound from "./button.mp3";
 import TapeSound from "./tape.mp3";
-import RainSound from "./rain.mp3"
-import TapeSound2 from "./tape2.wav"
+import RainSound from "./rain.mp3";
+import TapeSound2 from "./tape2.wav";
+import FireSound from "./fire.mp3";
+import LofiSound from "./lofi.mp3";
+import PianoSound from "./piano.mp3";
 
-export  {ButtonSound,TapeSound,RainSound,TapeSound2 };
+export  {ButtonSound,TapeSound,RainSound,TapeSound2,FireSound,LofiSound,PianoSound };
