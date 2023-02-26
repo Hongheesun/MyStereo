a5f0256eff7c0f96ddcd564975075a3241f9cd91
홍희선 <hia990618@gmail.com>
2023-02-26 22:20:55 +0900

    file: sound 및 video 파일 update

--- src/assets/sounds/index.js
+++ src/assets/sounds/index.js
@@ -1,4 +1,5 @@
 import ButtonSound from "./button.mp3";
 import TapeSound from "./tape.mp3";
+import RainSound from "./rain.mp3"
 
-export  {ButtonSound,TapeSound };
+export  {ButtonSound,TapeSound,RainSound };
--- src/assets/videos/index.js
+++ src/assets/videos/index.js
@@ -1,3 +1,9 @@
 import Rain from "./rain.mp4"
+import Fire from "./fire.mp4"
+import Cafe from "./cafe.mp4"
+import Cafe2 from "./cafe2.mp4"
+import CityLight from "./cityLight.mp4"
+import CityRain from "./cityRain.mp4"
+import Moon from "./moon.mp4"
 
-export { Rain };
+export { Rain,Fire,Cafe,Cafe2, CityLight,CityRain,Moon};
